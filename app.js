5a2a9776c31a76723bc531302636da82456d435d
Seu Nome <MendoncaGabriel1997@gmail.com>
2023-12-15 13:06:25 -0300

    add

--- app.js
+++ app.js
@@ -1,33 +1,20 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const video = document.getElementById('barcode-video');
-    const resultDiv = document.getElementById('barcode-result');
+const Quagga = require('quagga').default
 
-    Quagga.init({
-        inputStream: {
-            name: 'Live',
-            type: 'LiveStream',
-            target: video,
-            constraints: {
-                width: 640,
-                height: 480,
-                facingMode: 'environment',
-            },
-        },
-        decoder: {
-            readers: ['ean_reader'],
-        },
-    }, function (err) {
-        if (err) {
-            console.error(err);
-            return;
-        }
-        Quagga.start();
-    });
 
-    Quagga.onDetected(function (data) {
-        const code = data.codeResult.code;
-        resultDiv.textContent = 'Código de Barras: ' + code;
-        resultDiv.style.display = 'block';
-        // Aqui você pode adicionar lógica adicional com o código de barras detectado
-    });
-});
+Quagga.init({
+    inputStream : {
+      name : "Live",
+      type : "LiveStream",
+      target: document.querySelector('#yourElement')    // Or '#yourElement' (optional)
+    },
+    decoder : {
+      readers : ["code_128_reader"]
+    }
+  }, function(err) {
+      if (err) {
+          console.log(err);
+          return
+      }
+      console.log("Initialization finished. Ready to start");
+      Quagga.start();
+  });
